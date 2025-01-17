4eb4683df25fe7edcfc99ab0f22d6e6f85b2a409
microsoft <microsoft@sist>
2025-01-17 12:21:49 +0900

    1111

---
+++ NovelProject/src/common/Function.java
@@ -0,0 +1,17 @@
+package common;
+
+public class Function {
+
+  public static final int LOGIN = 100;
+  public static final int MYLOG = 110;
+
+  public static final int WAITCHAT = 200;
+
+  public static final int SENDMESSAGE = 300;
+
+  public static final int INFO = 400;
+
+  public static final int EXIT = 900;
+  public static final int MYEXIT = 910;
+
+}
