e9dd72bde5b1a57099f6608393fe29b7a196698d
sist_108 <sist_108@sist108>
2025-01-20 17:48:37 +0900

    20250120 17:58 뉴스 패널 생성

--- NovelProject/src/main/NewsPanel.java
+++ NovelProject/src/main/NewsPanel.java
@@ -8,16 +8,34 @@ import javax.swing.border.*;
 import common.*;
 import dao.*;
 import vo.*;
-public class NewsPanel extends JPanel {
+public class NewsPanel extends JPanel 
+implements ActionListener
+{
 	JTextField tf;
-	
+	JButton b;
+	JTextArea ta;
+
 	ControlPanel cp;
 	public NewsPanel(ControlPanel cp) {
 		this.cp = cp;
-		tf = new JTextField();
-		setLayout(null);
-		tf.setBounds(100, 100, 100, 30);
-		tf.setText("뉴스게시판");
-		add(tf);
+	   	setLayout(null);
+	   	 
+	   	b=new JButton("뉴스검색");
+	   	tf=new JTextField();
+	   	ta=new JTextArea();
+	   	JScrollPane js=new JScrollPane(ta);
+	   	tf.setBounds(20, 20, 650, 30);
+	   	b.setBounds(680, 20, 120, 30);
+	   	js.setBounds(20, 60, 780, 450);
+
+	   	add(tf); add(b); 
+	   	add(js);
+	   	
+	   	b.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
